2b05afc22553d6655a50896ba9ce00f2d43c780b
Lindsey Handley <lindseyhandley@gmail.com>
2022-01-14 15:06:42 -0800

    first iteration of website complete

--- kitsap-dev-website/src/App.js
+++ kitsap-dev-website/src/App.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 import logo from './logo.svg';
+import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import './App.css';
@@ -9,14 +13,18 @@ import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import InputAdornment from '@mui/material/InputAdornment';
 
+let bannerBackgroundColor = "#513BEB"
+let h2TagColor = "#513BEB"
+let jumbotronContrastingColor = "#FFFFFF"
+
 function TechLogo(props) {
   // Mouseover effect / tooltip for people to investigate/play with logos
   return (
-    <img src={"tech-logos/" + props.name + ".png"} height={60} style={{ margin: 15 }} />
+    <img src={"tech-logos/" + props.name + ".png"} height={80} style={{ margin: 15, filter: "grayscale(100%)" }} />
   )
 }
 
-function submitToAPI(e) {
+function submitToAPI(e, setFeedback, setFeedbackSeverityLevel) {
   e.preventDefault();
   let user_input = Array.from(e.target.parentNode.querySelectorAll("input")).map(e => e.value);
   let desc = e.target.parentNode.querySelector("textarea").value;
@@ -27,22 +35,20 @@ function submitToAPI(e) {
 
   var Namere = /[A-Za-z]{1}[A-Za-z]/;
   if (!Namere.test(name)) {
-    alert("Name can not less than 2 char");
-    return;
-  }
-  var mobilere = /[0-9]{10}/;
-  if (!mobilere.test(phone)) {
-    alert("Please enter valid mobile number");
+    setFeedback("Name must be longer than 2 characters.");
+    setFeedbackSeverityLevel("error");
     return;
   }
   if (email == "") {
-    alert("Please enter your email id");
+    setFeedback("Please enter your email address.");
+    setFeedbackSeverityLevel("error");
     return;
   }
 
   var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
   if (!reeamil.test(email)) {
-    alert("Please enter valid email address");
+    setFeedback("Please enter a valid email address.");
+    setFeedbackSeverityLevel("error");
     return;
   }
 
@@ -64,94 +70,141 @@ function submitToAPI(e) {
     body: JSON.stringify(data)
   }).then(function () {
     // clear form and show a success message
-    alert("Successfull");
-    document.getElementById("contact-form").reset();
-  }).catch(function () {
+    setFeedback("Thank you! We got your message. We'll get back to you soon.");
+    setFeedbackSeverityLevel("success");
+    Array.from(e.target.parentNode.querySelectorAll("input")).map(e => e.value = "");
+  }).catch(function (e) {
+    console.log(e)
     // show an error message
-    alert("UnSuccessfull");
+    setFeedback("Something went wrong. It's not your fault. You can email us at contact@thoughtstem.com.");
+    setFeedbackSeverityLevel("error");
   })
 
 }
 
-function App() {
-  return (
-    <div className="">
-      <Container style={{ width: "100%", maxWidth: "100%", position: "relative" }}>
-        <Box sx={{ height: '40vh', backgroundImage: "url(olympic-mountains-with-code.png)", backgroundPosition: "center", backgroundSize: "cover", height: "80vh", }} />
-        <div style={{ textAlign: "center", display: "inline-block", color: "black", zIndex: 10, width: "100%", position: "absolute", top: "50%", MsTransform: "translateY(-50%)", transform: "translateY(-50%)", left: 0 }}>
-          <div>
-            <Fade in={true} timeout={2000}>
-              <img src="kitsap-dev-logo.svg" style={{ height: "300px" }} />
-            </Fade>
-            <Fade in={true} timeout={4000}>
-            <h2 style={{ color: "white", fontFamily: "Arial, Helvetica, sans-sarif", fontSize: "25pt" }}>Software solutions for you</h2>
-            </Fade>
-          </div>
-        </div>
-      </Container>
-      <Container style={{ width: "100%", maxWidth: "100%" }}>
-        <div style={{ textAlign: "center" }}>
-          <h2>Technologies We Love</h2>
-          <div>
-            <TechLogo name="c_sharp" />
-            <TechLogo name="java" />
-            <TechLogo name="node" />
-            <TechLogo name="racket" />
-            <TechLogo name="stripe" />
-            <TechLogo name="shopify" />
-            <TechLogo name="linux" />
-            <TechLogo name="aws" />
-            <TechLogo name="iOS" />
-            <TechLogo name="android" />
-            <TechLogo name="electron" />
-            <TechLogo name="react" />
-            <TechLogo name="rails" />
-            <TechLogo name="rust" />
-            <TechLogo name="go" />
-            <TechLogo name="php" />
-            <TechLogo name="css" />
-            <TechLogo name="html" />
-            <TechLogo name="postgres" />
-            <TechLogo name="mysql" />
-            <TechLogo name="c++" />
-            <TechLogo name="python" />
-            <TechLogo name="ruby" />
-            <TechLogo name="javascript" />
-          </div>
-        </div>
-      </Container>
-    <Container style={{paddingTop:30, paddingBottom: 50, width:"100%",maxWidth:"100%",backgroundColor: '#DDDDDD' }}>
-      <div style={{textAlign:"center", }}>        
-        <h2 style={{paddingBottom:20}}>Let Us Help You With Your Project</h2>
-        <FormControl id="contact-form" method="post" style={{position: "relative"}}>
-          <Input type="text" style={{height:35}} placeholder="NAME" className="form-control name" startAdornment={
+const FormContainer = styled('div')(({theme}) => ({
+    [theme.breakpoints.down('sm')]: {
+      width: "80%",
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: "50%",
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: "30%",
+    }
+  }))
+
+function ContactFormSection(props){
+  let [feedback, setFeedback] = useState("")
+  let [feedbackSeverityLevel, setFeedbackSeverityLevel] = useState("info")
+
+  return(
+  
+    <Container style={{paddingTop:30, paddingBottom: 50, width:"100%",maxWidth:"100%",backgroundColor:jumbotronContrastingColor }}>
+    <FormContainer id="contact-form" style={{textAlign:"center", fontSize: "0.875em", color: "#000000", margin: "auto" }}>
+        <StyledH2 style={{paddingBottom:20, color: h2TagColor}}>Need Help?</StyledH2>
+        {feedback=="" ? "": <Alert severity={feedbackSeverityLevel} style={{marginBottom: 20}}>{feedback}</Alert>}
+          <Input type="text" style={{height:35, width:"100%"}} placeholder="NAME" className="form-control name" startAdornment={
             <InputAdornment position="start">
               <img src="./icons/name.svg" style={{ width: 20, paddingRight: 3}}/>
             </InputAdornment>
           }/>
-          <Input type="phone" style={{height:35}} placeholder="PHONE" className="form-control phone" startAdornment={
+          <Box pb={2.5} />
+          <Input type="phone" style={{height:35, width:"100%"}} placeholder="PHONE" className="form-control phone" startAdornment={
             <InputAdornment position="start">
               <img src="./icons/phone.svg" style={{ width: 20, padding: 4}}/>
             </InputAdornment>
     }
     />
-          <Input type="email" style={{height:35}} placeholder="EMAIL" className="form-control email" startAdornment={
+          <Box pb={2.5} />
+          <Input type="email" style={{height:35, width:"100%"}} placeholder="EMAIL" className="form-control email" startAdornment={
             <InputAdornment position="start">
               <img src="./icons/email.svg" style={{ width: 20, padding: 3}}/>
             </InputAdornment>
     }
     />
+          <Box pb={2.5} />
           <TextField multiline id="description-input" variant="standard" placeholder="WHAT CAN WE DO FOR YOU?" className="form-control description" style={{width:"100%"}} InputProps={{startAdornment: (
             <InputAdornment position="start">
               <img src="./icons/description.svg" style={{width: 20, padding: 3}}/>
             </InputAdornment>
           )}}
     />
-          <Button type="submit" onClick={(event) => submitToAPI(event)} id="submit" className="btn btn-lg" variant="outline">SEND</Button>
-        </FormControl>
-      </div>
+          <Button type="submit" onClick={(event) => submitToAPI(event, setFeedback, setFeedbackSeverityLevel)} id="submit" className="btn btn-lg" variant="outline" style={{width:"100%"}}>SEND</Button>
+      </FormContainer>
     </Container>
-  </div>
+)
+}
+
+function TechnologiesSection(props) {
+
+  return(
+      <Container style={{ width: "100%", maxWidth: "100%" }}>
+        <div style={{ textAlign: "center" }}>
+          <h2 style={{color: h2TagColor}}>Technologies We Love</h2>
+          <div>
+    {/* <TechLogo name="node" />
+            <TechLogo name="stripe" />
+            <TechLogo name="shopify" />
+            <TechLogo name="aws" />
+            <TechLogo name="iOS" />
+            <TechLogo name="android" />
+            <TechLogo name="rails" />
+            <TechLogo name="go" />
+            <TechLogo name="php" />
+            <TechLogo name="mysql" /> */}
+            <TechLogo name="c++" />
+            <TechLogo name="c_sharp" />
+            <TechLogo name="rust" />
+            <TechLogo name="postgres" />
+            <TechLogo name="racket" />
+            <TechLogo name="css" />
+            <TechLogo name="html" />
+            <TechLogo name="java" />
+            <TechLogo name="linux" />
+            <TechLogo name="electron" />
+            <TechLogo name="react" />
+            <TechLogo name="python" />
+            <TechLogo name="ruby" />
+            <TechLogo name="javascript" />
+          </div>
+        </div>
+      </Container>
+  
+  )
+}
+
+const StyledH2 = styled('h2')(({theme}) => ({
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 20,
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: 25,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 25,
+    }
+  }))
+
+function App() {
+  
+  return (
+    <div className="">
+      <Container style={{ width: "100%", maxWidth: "100%", position: "relative" }}>
+        <Box sx={{ height: '40vh', backgroundColor:bannerBackgroundColor, backgroundPosition: "center", backgroundSize: "cover", height: "80vh", minHeight: "500px" }} />
+        <div style={{ textAlign: "center", display: "inline-block", color: "black", zIndex: 10, width: "100%", position: "absolute", top: "50%", MsTransform: "translateY(-50%)", transform: "translateY(-50%)", left: 0 }}>
+          <div>
+            <Fade in={true} timeout={2000}>
+              <img src="kitsap-dev-logo.svg" style={{ width:"80%", maxWidth:"500px" }} />
+            </Fade>
+            <Fade in={true} timeout={4000}>
+            <StyledH2 style={{ color: "white", fontFamily: "Arial, Helvetica, sans-sarif"}}>Software solutions for you</StyledH2>
+            </Fade>
+          </div>
+        </div>
+      </Container>
+    <ContactFormSection/>
+    </div>
   );
 }
 
